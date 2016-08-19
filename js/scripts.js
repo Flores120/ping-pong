a9b53dfd3b0827ac8aa7a150239a98549ece00f0
Alvaro Flores <alvaroflores@Alvaros-MacBook-Pro.local>
2016-08-19 09:08:41 -0700

    the progarm now spits back numbers

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@ var pingPong = function(numbers){
 	// debugger;
 	var words = ['ping', 'pong', 'ping-pong']
 	var input = [];
-	for (var i = 0; i <= input.length; i ++){
-		if (numbers % 3){
-			return words['ping']
+	for (var i = 0; i <= numbers; i ++){
+		if (number % 3 === 0){
+			alert(words);
 		}
 	}
-		return numbers
+	return numbers
 }
 // -------------->front end work<---------------
 $(document).ready(function(){
 	$('form#ping').submit(function(event){
 		var userInput = parseInt($('input#number').val());
 		var results = pingPong(userInput);
-		$('.results').append('<div>' + results + '</div>');
+		$('.results').append('<p>' + results + '</p>');
 		event.preventDefault();
 
 	});
