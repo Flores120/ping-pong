537f4c9cc6f0af305fc63db2fd244decd760f1b5
Alvaro Flores <alvaroflores@Alvaros-MacBook-Pro.local>
2016-08-19 07:51:42 -0700

    added the basic html layout and javascript basics

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var pingPong = function(numbers){
+	var output = [];
+	return output
+
+}
+
+// -------------->front end work<---------------
+$(document).ready(function(){
+	$('form#ping').submit(function(event){
+		var userInput = parseInt($('input#number').val());
+		event.preventDefault();
+
+	});
+});
