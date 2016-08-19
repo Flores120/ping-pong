6e1adfb677beefb5c5d74ab7cccd31478ad685ab
Alvaro Flores <alvaroflores@Alvaros-MacBook-Pro.local>
2016-08-19 08:59:21 -0700

    added more javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 var pingPong = function(numbers){
-	var output = [];
-	return output
-
+	// debugger;
+	var words = ['ping', 'pong', 'ping-pong']
+	var input = [];
+	for (var i = 0; i <= input.length; i ++){
+		if (numbers % 3){
+			return words['ping']
+		}
+	}
+		return numbers
 }
-
 // -------------->front end work<---------------
 $(document).ready(function(){
 	$('form#ping').submit(function(event){
 		var userInput = parseInt($('input#number').val());
+		var results = pingPong(userInput);
+		$('.results').append('<div>' + results + '</div>');
 		event.preventDefault();
 
 	});
